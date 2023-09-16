903ba3dcbc9d24b4ee550f46828ec81b78d73494
b1ruk <biruk3870@gmail.com>
2023-09-16 03:57:15 -0300

    media attachment feature

--- src/main/java/io/b1ruk/start/whatsAppS/service/message/MessageService.java
+++ src/main/java/io/b1ruk/start/whatsAppS/service/message/MessageService.java
@@ -1,11 +1,12 @@
 package io.b1ruk.start.whatsAppS.service.message;
 
+import com.whatsapp.api.domain.messages.ImageMessage;
 import com.whatsapp.api.domain.messages.TextMessage;
+import com.whatsapp.api.domain.messages.VideoMessage;
+import com.whatsapp.api.domain.messages.response.MessageResponse;
 import com.whatsapp.api.impl.WhatsappBusinessCloudApi;
 import io.b1ruk.start.whatsAppS.controller.dto.MessageDTO;
-import io.b1ruk.start.whatsAppS.entity.ChatroomEntity;
-import io.b1ruk.start.whatsAppS.entity.Message;
-import io.b1ruk.start.whatsAppS.entity.Participant;
+import io.b1ruk.start.whatsAppS.entity.*;
 import io.b1ruk.start.whatsAppS.repository.ChatroomRepository;
 import io.b1ruk.start.whatsAppS.repository.MediaAttachmentRepository;
 import io.b1ruk.start.whatsAppS.repository.MessageRepository;
@@ -40,7 +41,7 @@ public class MessageService {
         Participant participantModel = participantRepository.findById(messageDTO.getSenderId()).orElseThrow(() -> new RuntimeException("Participant not found"));
         ChatroomEntity chatroomEntity = chatroomRepository.findById(messageDTO.getChatroomId()).orElseThrow(() -> new RuntimeException("chatroom not found"));
 
-        var message= Message.builder()
+        var message = Message.builder()
                 .text(messageDTO.getText())
                 .chatroomId(messageDTO.getChatroomId())
                 .senderId(messageDTO.getSenderId())
@@ -49,10 +50,10 @@ public class MessageService {
 
         messageRepository.save(message);
 
-        if (!messageDTO.getMediaAttachments().isEmpty()){
-            //TODO save media attachments
-        }else {
-            sendMessage(messageDTO,participantModel,chatroomEntity);
+        if (!messageDTO.getMediaAttachments().isEmpty()) {
+            sendMediaAttachments(messageDTO, participantModel, chatroomEntity);
+        } else {
+            sendMessage(messageDTO, participantModel, chatroomEntity);
         }
 
         messageDTO.setId(message.getId());
@@ -60,12 +61,53 @@ public class MessageService {
         return ResponseEntity.ok().body(messageDTO);
     }
 
-    public void sendMessage(MessageDTO messageDTO, Participant participantModel, ChatroomEntity chatroomEntity){
+    private void sendMediaAttachments(MessageDTO messageDTO, Participant participantModel, ChatroomEntity chatroomEntity) {
+        messageDTO.getMediaAttachments()
+                .forEach(mediaAttachment -> {
+                    if (mediaAttachment.getType().equals(MediaType.PICTURE)) {
+                        sendImage(mediaAttachment, participantModel, chatroomEntity);
+                    } else {
+                        sendVideo(mediaAttachment, participantModel, chatroomEntity);
+                    }
+                });
+    }
+
+    private void sendImage(MediaAttachment mediaAttachment, Participant participantModel, ChatroomEntity chatroomEntity) {
+
+        MediaAttachment attachmentModel = mediaAttachmentRepository.save(mediaAttachment);
+
+        var imageMessage = new ImageMessage()
+                .setId(attachmentModel.getId().toString());
+
+        var message = com.whatsapp.api.domain.messages.Message.MessageBuilder.builder()
+                .setTo(participantModel.getPhoneId())
+                .buildImageMessage(imageMessage);
+
+
+        MessageResponse messageResponse = whatsappBusinessCloudApi.sendMessage(chatroomEntity.getName(), message);
+    }
+
+    private void sendVideo(MediaAttachment mediaAttachment, Participant participantModel, ChatroomEntity chatroomEntity) {
+
+        MediaAttachment attachmentModel = mediaAttachmentRepository.save(mediaAttachment);
+
+        var videoMessage = new VideoMessage()
+                .setId(attachmentModel.getId().toString());
 
         var message = com.whatsapp.api.domain.messages.Message.MessageBuilder.builder()//
-                .setTo(participantModel.getPhoneId())//
-                .buildTextMessage(new TextMessage()//
-                        .setBody(messageDTO.getText())//
+                .setTo(participantModel.getPhoneId())
+                .buildVideoMessage(videoMessage);
+
+
+        MessageResponse messageResponse = whatsappBusinessCloudApi.sendMessage(chatroomEntity.getName(), message)
+    }
+
+    public void sendMessage(MessageDTO messageDTO, Participant participantModel, ChatroomEntity chatroomEntity) {
+
+        var message = com.whatsapp.api.domain.messages.Message.MessageBuilder.builder()
+                .setTo(participantModel.getPhoneId())
+                .buildTextMessage(new TextMessage()
+                        .setBody(messageDTO.getText())
                         .setPreviewUrl(false));
 
 
